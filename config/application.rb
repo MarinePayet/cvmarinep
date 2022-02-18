7f3bdb095b8e1702d663f136c8ffeb5506c9d3ac
Marine Payet <65187393+marineP974@users.noreply.github.com>
2022-02-18 22:45:24 +0100

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -12,11 +12,11 @@ module CVmarinePayet
       generate.assets false
       generate.helper false
       generate.test_framework :test_unit, fixture: false
-      config.assets.initialize_on_precompile false
+     
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
